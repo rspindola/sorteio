424d5e032c7fb2da803046fee5a34385a7bcbe29
Renato <rspindola@live.com>
2018-10-30 16:06:09 -0300

    bug

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -46,6 +46,7 @@ class ItemController extends Controller
             $imagem = $request->imagem;
             $filename = 'thumb-'.str_random(10).time().'.'.$imagem->getClientOriginalExtension(); 
             $destinationPath = public_path('images/thumb');
+            return dd('ok');
             $thumb_img = Image::make($imagem->getRealPath())->resize(795, 550);
             $thumb_img->save($destinationPath.'/'.$filename,80);
             
